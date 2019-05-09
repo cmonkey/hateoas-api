ce7855ff263513dbb771dfdcaed49ffa972d2f5e
CMonkey <42.codemonkey@gmail.com>
2019-05-09 15:14:31 +0800

    add PersonController

--- src/main/scala/org/excavator/hateoas/api/controller/PersonController.scala
+++ src/main/scala/org/excavator/hateoas/api/controller/PersonController.scala
@@ -1,10 +1,17 @@
 package org.excavator.hateoas.api.controller
 
-import org.excavator.hateoas.api.entity.{Person, PersonResource}
+import java.util
+import java.util.stream.Collectors
+
+import org.excavator.hateoas.api.entity.Person
+import org.excavator.hateoas.api.exception.NotFoundException
 import org.excavator.hateoas.api.repository.PersonRepository
-import org.springframework.hateoas.Resources
-import org.springframework.http.ResponseEntity
+import org.excavator.hateoas.api.resource.PersonResource
+import org.springframework.hateoas.{Link, Resources}
+import org.springframework.http.{HttpStatus, ResponseEntity}
 import org.springframework.web.bind.annotation.{DeleteMapping, GetMapping, PathVariable, PostMapping, PutMapping, RequestBody, RequestMapping, RestController}
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping(value = Array("/people"), produces = Array("application/hal+json"))
@@ -12,26 +19,75 @@ class PersonController(val personRepository: PersonRepository) {
 
   @GetMapping
   def all(): ResponseEntity[Resources[PersonResource]] = {
-    // Get all
+    val collection:java.util.List[PersonResource] = new util.ArrayList[PersonResource]()
+
+      personRepository.findAll()
+      .stream().forEach(person => {
+        collection.add(new PersonResource(person))
+      })
+
+    val resources = new Resources[PersonResource](collection)
+
+    val uriString = ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString
+    resources.add(new Link(uriString, "self"))
+    ResponseEntity.ok(resources)
   }
 
-  @GetMapping("/{id}")
+  @GetMapping(Array("/{id}"))
   def get(@PathVariable id: Long): ResponseEntity[PersonResource] = {
 
+    val optional = personRepository.findById(id)
+
+    var r: ResponseEntity[PersonResource] = null
+
+    if(optional.isPresent){
+      r = ResponseEntity.ok(new PersonResource(optional.get()))
+    }else{
+      new NotFoundException(id)
+    }
+
+    r
   }
 
   @PostMapping
   def post(@RequestBody person: Person): ResponseEntity[PersonResource] = {
 
+    personRepository.save(person)
+
+    val uri = MvcUriComponentsBuilder.fromController(getClass)
+      .path("/{id}")
+      .buildAndExpand(person.id.asInstanceOf[Object])
+      .toUri
+
+    ResponseEntity.created(uri).body(new PersonResource(person))
+
   }
 
-  @PutMapping("/{id}")
+  @PutMapping(Array("/{id}"))
   def put(@PathVariable id: Long, @RequestBody person: Person): ResponseEntity[PersonResource] = {
 
+    person.copy(id)
+    personRepository.save(person)
+
+    val resource = new PersonResource(person)
+
+    val uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri
+
+    ResponseEntity.created(uri).body(resource)
   }
 
-  @DeleteMapping("/{id}")
-  def delete(@PathVariable id:Long): ResponseEntity[_] = {
+  @DeleteMapping(Array("/{id}"))
+  def delete(@PathVariable id:Long): ResponseEntity[Any] = {
 
+    val optional = personRepository.findById(id)
+    var r: ResponseEntity[Any] = null
+    if(optional.isPresent){
+      personRepository.deleteById(id)
+      r = ResponseEntity.noContent().build()
+    }else{
+      new NotFoundException(id)
+    }
+
+    r
   }
 }
