e8fdb88dd158fd5be60a1c083061751b9886b2ce
CMonkey <42.codemonkey@gmail.com>
2019-05-09 15:46:01 +0800

    change PersonResource

--- src/main/scala/org/excavator/hateoas/api/resource/PersonResource.scala
+++ src/main/scala/org/excavator/hateoas/api/resource/PersonResource.scala
@@ -1,23 +1,11 @@
 package org.excavator.hateoas.api.resource;
 
-import lombok.Getter;
-import org.excavator.hateoas.api.controller.PersonController;
-import org.excavator.hateoas.api.entity.Person;
-import org.springframework.hateoas.ResourceSupport;
+import org.excavator.hateoas.api.controller.PersonController
+import org.excavator.hateoas.api.entity.Person
+import org.springframework.hateoas.ResourceSupport
+import org.springframework.hateoas.mvc.ControllerLinkBuilder._
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-@Getter
-public class PersonResource extends ResourceSupport {
-
-    private Person person;
-
-    public PersonResource(Person person) {
-        this.person = person;
-        long id = person.id();
-
-        add(linkTo(PersonController.class).withRel("people"));
-        add(linkTo(methodOn(PersonController.class).get(id)).withSelfRel());
-    }
+class PersonResource(person: Person) extends ResourceSupport {
+      add(linkTo(classOf[PersonController]).withRel("people"));
+      add(linkTo(methodOn(classOf[PersonController]).get(person.id)).withSelfRel());
 }
