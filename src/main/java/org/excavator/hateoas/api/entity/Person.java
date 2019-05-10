13fbd9b3444cc143b267c301e5ea517ab7a33844
CMonkey <42.codemonkey@gmail.com>
2019-05-10 10:15:12 +0800

    change Person in methods

--- src/main/java/org/excavator/hateoas/api/entity/Person.java
+++ src/main/java/org/excavator/hateoas/api/entity/Person.java
@@ -1,14 +1,7 @@
-package org.excavator.hateoas.api.exception;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+package org.excavator.hateoas.api.entity;
 
 import java.time.Duration;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Person {
     private long id;
     private String firstName;
@@ -16,4 +9,64 @@ public class Person {
     private Duration dateOfBirth;
     private String profession;
     private int salary;
+
+    public Person() {
+    }
+
+    public Person(long id, String firstName, String secondName, Duration dateOfBirth, String profession, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.dateOfBirth = dateOfBirth;
+        this.profession = profession;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Duration getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Duration dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
